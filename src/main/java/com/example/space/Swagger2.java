a725fbd5050520f9fed1a4169f0aaba2de94feb3
liyu <liyu@swifthealth.cn>
2018-07-26 16:29:45 +0800

    create api doc by swagger

---
+++ src/main/java/com/example/space/Swagger2.java
@@ -0,0 +1,32 @@
+package com.example.space;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author liyu
+ * @date 18-7-26
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("spring boot tutorial RESTful APIs")
+                        .description("springboot API")
+                        .contact("aspirin")
+                        .version("1.0")
+                        .build()
+                ).select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.space.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
--- src/main/java/com/example/space/web/UserController.java
+++ src/main/java/com/example/space/web/UserController.java
@@ -1,6 +1,9 @@
 package com.example.space.web;
 
 import com.example.space.domain.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -15,23 +18,33 @@ public class UserController {
 
     static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
 
+    @ApiOperation(value = "获取用户列表", notes = "")
     @GetMapping("/")
     public List<User> getUserList(){
         List<User> r = new ArrayList<User>(users.values());
         return r;
     }
 
+    @ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @PostMapping("/")
     public String postUser(@ModelAttribute User user){
         users.put(user.getId(), user);
         return "success";
     }
 
+    @ApiOperation(value = "更新用户详细信息", notes = "根据id获取用户 ")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @GetMapping("{id}")
     public User getUser(@PathVariable Long id){
         return users.get(id);
     }
 
+    @ApiOperation(value = "更新用户", notes = "根据id更新用户")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户实体user", required = true, dataType = "User")
+    })
     @PutMapping("{id}")
     public String putUser(@PathVariable Long id, @ModelAttribute User user){
         User u = users.get(id);
@@ -41,6 +54,8 @@ public class UserController {
         return "success";
     }
 
+    @ApiOperation(value = "删除用户", notes = "根据id删除用户")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @DeleteMapping
     public String deleteUser(@PathVariable Long id) {
         users.remove(id);
