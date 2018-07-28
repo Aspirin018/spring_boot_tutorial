871eb3d85d7348a3af34f4b28fe1a3a7ec6b4462
liyu <liyu@swifthealth.cn>
2018-07-28 11:54:07 +0800

    async tasks

--- src/main/java/com/example/space/Application.java
+++ src/main/java/com/example/space/Application.java
@@ -3,6 +3,7 @@ package com.example.space;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling //启用定时任务
+@EnableAsync //开启异步调用
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/example/space/framework/asyncTask/Task.java
+++ src/main/java/com/example/space/framework/asyncTask/Task.java
@@ -1,5 +1,6 @@
 package com.example.space.framework.asyncTask;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
@@ -13,6 +14,8 @@ public class Task {
 
     public static Random random = new Random();
 
+    @Async
+    //@Async定义异步方法不可以用static修饰，否则异步调用不生效
     public void doTaskOne() throws InterruptedException {
         System.out.println("开始任务一");
         long start = System.currentTimeMillis();
@@ -21,6 +24,7 @@ public class Task {
         System.out.println("完成任务一，耗时：" + (end - start) + "毫秒");
     }
 
+    @Async
     public void doTaskTwo() throws InterruptedException {
         System.out.println("开始任务二");
         long start = System.currentTimeMillis();
@@ -29,6 +33,7 @@ public class Task {
         System.out.println("完成任务二，耗时：" + (end - start) + "毫秒");
     }
 
+    @Async
     public void doTaskThree() throws InterruptedException {
         System.out.println("开始任务三");
         long start = System.currentTimeMillis();
