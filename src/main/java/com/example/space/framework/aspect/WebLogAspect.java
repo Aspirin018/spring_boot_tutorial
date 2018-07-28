1657cf682c9a33f5fe7d02f6435af2f9cc61b29f
liyu <liyu@swifthealth.cn>
2018-07-28 17:57:21 +0800

    aop sync & aop priority

--- src/main/java/com/example/space/framework/aspect/WebLogAspect.java
+++ src/main/java/com/example/space/framework/aspect/WebLogAspect.java
@@ -6,7 +6,7 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -20,11 +20,26 @@ import java.util.Arrays;
  * @author liyu
  * @date 18-7-28
  */
+/**
+ * @Order定义每个切面的优先级，我们需要@Order(i)注解来标识切面的优先级。i的值越小，优先级越高。
+ * 在@Before中优先执行@Order(5)的内容，再执行@Order(10)的内容
+ * 在@After和@AfterReturning中优先执行@Order(10)的内容，再执行@Order(5)的内容
+ */
 @Aspect
 @Component
+@Order(5)
 public class WebLogAspect {
 
     private Logger logger = Logger.getLogger(getClass());
+
+    /**
+     * 在WebLogAspect切面中，分别通过doBefore和doAfterReturning两个独立函数实现了切点头部和切点返回后执行的内容，
+     * 若我们想统计请求的处理时间，就需要在doBefore处记录时间，并在doAfterReturning处通过当前时间与开始处记录的时间计算得到请求处理的消耗时间。
+     * 那么我们是否可以在WebLogAspect切面中定义一个成员变量来给doBefore和doAfterReturning一起访问呢？是否会有同步问题呢？
+     * 的确，直接在这里定义基本类型会有同步问题，所以我们可以引入ThreadLocal对象
+     * ThreadLocal用于保存某个线程共享变量：对于同一个static ThreadLocal，不同线程只能从中get，set，remove自己的变量，而不会影响其他线程的变量。
+     */
+    ThreadLocal<Long> startTime = new ThreadLocal<>();
 
     @Pointcut("execution(public * com.example.space.api.web..*.*(..))")
     public void webLog(){}
@@ -32,6 +47,9 @@ public class WebLogAspect {
 
     @Before("webLog()")
     public void doBefore(JoinPoint joinPoint){
+
+        startTime.set(System.currentTimeMillis());
+
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
 
@@ -45,5 +63,6 @@ public class WebLogAspect {
     @AfterReturning(returning = "ret", pointcut = "webLog()")
     public void doAfterReturing(Object ret) {
         logger.info("RESPONSE:" + ret);
+        logger.info("SPEND TIME:" + (System.currentTimeMillis() - startTime.get()));
     }
 }
